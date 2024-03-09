3658d165291a086977efa4823419c2af651851e6
Kostyantin <podyankee@gmail.com>
2024-03-09 05:42:41 +0300

    day-14 home page products slider

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -136,6 +136,22 @@ jQuery(function ($) {
 		});
 	}
 
+	// Products Slider
+	if ($('.alekids_product_slider').length) {
+		let columns = $('.alekids_product_slider .products').data('columns');
+
+		$('.alekids_product_slider .products').slick({
+			infinite: true,
+			autoplay: false,
+			arrows: true,
+			slidesToShow: columns,
+			slidesToScroll: 1,
+			dots: false,
+			prevArrow: '<button type="button" class="slick-prev"></button>',
+			nextArrow: '<button type="button" class="slick-next"></button>',
+		});
+	}
+
 	// Init Slick Slider
 
 	if ($('.alekids_post_gallery'.length)) {
--- woocommerce/loop/loop-start.php
+++ woocommerce/loop/loop-start.php
@@ -19,4 +19,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<div class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+<div class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>" data-columns="<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
